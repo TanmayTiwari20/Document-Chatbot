a9cb1d5758576aa57f47ffc76045dfc32a383072
Tanmay Tiwari <spes1dts@gmail.com>
2024-02-23 20:49:43 +0530

    inital commit

---
+++ chatdoc.py
@@ -0,0 +1,10 @@
+import os
+from apikey import apikey
+
+import streamlit as st
+from langchain import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
